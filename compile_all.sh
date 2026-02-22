7a8efb1a14cd661b6a7f7fcd927cf1f7ba0c4223
Yehor Korotenko <dobbikov@gmail.com>
2026-02-22 13:33:38 +0100

    added stats-cheatsheet

--- compile_all.sh
+++ compile_all.sh
@@ -1,6 +1,8 @@
 #!/usr/local/bin
 typst compile ode-prep-patiel/main.typ
 typst compile stats/main.typ
+pdflatex stats-cheatsheet/main.tex
 
 mv ode-prep-patiel/main.pdf docs/ode-prep-partiel-td.pdf
 mv stats/main.pdf docs/stats.pdf
+mv stats-cheatsheet/main.pdf docs/stats-cheatsheet.pdf
